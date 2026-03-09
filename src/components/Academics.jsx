45b2acbdb0b890d6456ec6f4c45024bad053dce0
Phạm Nguyễn Phú Thành <thanhphamnguyenphu2k5@gmail.com>
2026-03-09 20:04:03 +0700

    Fix(Project): fix images url

--- src/components/Academics.jsx
+++ src/components/Academics.jsx
@@ -82,7 +82,7 @@ EducationCard.displayName = "EducationCard";
 // --- Static Data (No changes needed) ---
 const ACADEMICS_DATA = [
   {
-    logo: "/assets/logos/SGU-LOGO.png",
+    logo: "@/assets/logos/SGU-LOGO.png",
     alt: "SGU Logo",
     title: "Sai Gon University",
     link: "https://www.sgu.edu.vn/",
@@ -92,7 +92,7 @@ const ACADEMICS_DATA = [
     score: "8.51 / 10",
   },
   {
-    logo: "/assets/logos/THPTMacinhChi-1749811237-1749811252.jpg", // Đổi tên file cho gọn
+    logo: "@/assets/logos/THPTMacinhChi-1749811237-1749811252.jpg", // Đổi tên file cho gọn
     alt: "Mac Dinh Chi High School Logo",
     title: "Mac Dinh Chi High School",
     link: "https://thptmacdinhchi.hcm.edu.vn/",
